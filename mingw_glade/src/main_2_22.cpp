75721b9bc1bc13d0cef3a258393f5af4021e2e3a
Ronan Trpos <Ronan.Trepos@toulouse.inra.fr>
2013-04-16 11:03:05 +0200

    replace mingw_gtkmm by mingw_glade:
    Error is due to glade requiring newer version of gtk+ than the one
    provided for win32

---
+++ mingw_glade/src/main_2_22.cpp
@@ -0,0 +1,24 @@
+#include <gtkmm.h>
+#include <iostream>
+
+int main (int argc, char **argv)
+{
+    Gtk::Main kit(argc, argv);
+    std::string file_path("c:\\tests\\mingw_glade\\data\\toto_2_22.glade");
+    Glib::RefPtr<Gtk::Builder> refBuilder = Gtk::Builder::create();
+    try {
+        refBuilder->add_from_file(file_path);
+    }
+    catch(const Glib::FileError& ex)
+    {
+        std::cerr << "FileError: " << ex.what() << std::endl;
+        return 1;
+    }
+    catch(const Gtk::BuilderError& ex)
+    {
+        std::cerr << "BuilderError: " << ex.what() << std::endl;
+        return 1;
+    }
+    //will work because win gtkmm binaries are 2.22 and glade requires 2.22
+    return 0;
+}
